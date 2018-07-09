b38140e50194347390337c0c5b95c6b6c512a47c
liu tao <465275024@qq.com>
2018-07-09 16:14:55 +0800

    1

--- js/index.js
+++ js/index.js
@@ -15,36 +15,9 @@ import 'swiper/dist/css/swiper.css';
 import './autoWidth';
 window.reset_vw_base(780);
 
-document.onreadystatechange = function () {
-  if (document.readyState == "complete") {
-      document.body.style.display = "block";
-      // console.log("加载完成");
-  } else {
-      document.body.style.display = "none";
-  }
-};
-// var mySwiper1 = new Swiper('.swiper-container1', {
-//   autoplay: true,//可选选项，自动滑动
-//   loop : true,
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
-  
-// });
-// var mySwiper2 = new Swiper('.swiper-container2', {
-//   grabCursor:true,
-//   autoplay: true,//可选选项，自动滑动
-//   loop : true,
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
-//   controller: {
-//     control:mySwiper1,
-//     inverse: true,
-//   },
-// });
+
 var galleryTop = new Swiper('.gallery-top', {
-  spaceBetween: 10,
+  // spaceBetween: 100,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
@@ -53,21 +26,19 @@ var galleryTop = new Swiper('.gallery-top', {
 var galleryThumbs = new Swiper('.gallery-thumbs', {
   spaceBetween: 10,
   centeredSlides: true,
-  // slidesPerView: 'auto',
+  slidesPerView: 5,
+  // slidesPerGroup: 3,
   touchRatio: 0.2,
   slideToClickedSlide: true,
+  freeMode: true,
+  // by: 'container',
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
 galleryTop.controller.control = galleryThumbs;
 galleryThumbs.controller.control = galleryTop;
-  // layui.use('carousel', function(){
-  //   var carousel = layui.carousel;
-  //   //建造实例
-  //   carousel.render({
-  //     elem: '#test1'
-  //     ,width: '100%' //设置容器宽度
-  //     ,arrow: 'always' //始终显示箭头
-  //     //,anim: 'updown' //切换动画方式
-  //   });
-  // });
+
 
   
